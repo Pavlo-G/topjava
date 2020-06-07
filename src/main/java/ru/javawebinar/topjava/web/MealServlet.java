a961541b37479b12a9915d4031e00fe42f1c13a2
Pavlo-G <6306260@gmail.com>
2020-06-07 12:36:38 +0400

    HW1_add_meal_Servlet_meal.jsp

---
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -0,0 +1,48 @@
+package ru.javawebinar.topjava.web;
+
+import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+public class MealServlet extends HttpServlet {
+    private static final Logger log = getLogger(MealServlet.class);
+
+    //    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//
+//    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        log.debug("redirect to meals");
+       // response.sendRedirect("meals.jsp");
+        List<Meal> meals = Arrays.asList(
+                new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500),
+                new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000),
+                new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500),
+                new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100),
+                new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000),
+                new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500),
+                new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410),
+                new Meal(LocalDateTime.of(2020, Month.FEBRUARY, 1, 15, 0), "Сур", 310),
+                new Meal(LocalDateTime.of(2020, Month.FEBRUARY, 12, 20, 0), "Пельмени", 510),
+                new Meal(LocalDateTime.of(2020, Month.FEBRUARY, 24, 20, 0), "Ужин Торт", 750)
+        );
+
+        List<MealTo> mealsTo = MealsUtil.filteredByStreams(meals, LocalTime.MIN,LocalTime.MAX,2000);
+        request.setAttribute("mealsList",mealsTo);
+        request.getRequestDispatcher("meals.jsp").forward(request, response);
+    }
+}
---
+++ src/main/webapp/meals.jsp
@@ -0,0 +1,63 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Pasha
+  Date: 06.06.2020
+  Time: 15:28
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="tags" tagdir="/WEB-INF/tags" %>
+
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Meals</title>
+</head>
+<body>
+<h3><a href="index.html">Home</a></h3>
+<hr>
+<h2>Meals</h2>
+
+<table class="table table-bordered table-striped">
+    <thead  class="thead-dark">
+    <tr>
+        <th scope="col"> Date and Time</th>
+        <th scope="col">Description</th>
+        <th scope="col">Calories</th>
+        <th scope="col">Excess</th>
+    </tr>
+    </thead>
+    <tbody>
+    <jsp:useBean id="mealsList" scope="request" type="java.util.List"/>
+    <c:forEach var="meal" items="${mealsList}">
+
+
+        <tr style="color:${meal.excess ? 'red' : 'green'}">
+            <td>
+                <tags:localDate date="${meal.dateTime}"/>
+            </td>
+            <td><c:out value="${meal.description}"/></td>
+            <td><c:out value="${meal.calories}"/></td>
+            <td><c:out value="${meal.excess}"/></td>
+
+        </tr>
+    </c:forEach>
+    </tbody>
+</table>
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+</body>
+</html>
